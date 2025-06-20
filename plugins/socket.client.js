dad70e4fe856b37f5114d1ecf3f3cab00051f8cf
Donald Zou <donaldzou@live.hk>
2025-06-21 01:45:55 +0800

    Fixed socket issue. Using express instead of Nitro

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -1,10 +1,13 @@
 import { io } from "socket.io-client"
+import {v4} from "uuid";
+import dayjs from "dayjs";
 
 export default defineNuxtPlugin(() => {
-	const socket = io()
+	const socket = io(`${document.location.hostname}:3001`)
+	console.log(socket)
 	return {
 		provide: {
 			socket: socket
 		}
 	}
-})
+});
--- server/plugins/socket.io.ts
+++ server/plugins/socket.io.ts
@@ -4,32 +4,57 @@ import { Server } from "socket.io";
 import { defineEventHandler } from "h3";
 import { setIO } from '../utils/socket'
 
+import express from "express";
+import { createServer } from "http";
+
 export default defineNitroPlugin((nitroApp: NitroApp) => {
-    const engine = new Engine();
-    const io = new Server();
+    // const engine = new Engine();
+    // const io = new Server();
+    //
+    // io.bind(engine);
+    //
+    // io.on("connection", (socket) => {
+    //     console.log("Connected Socket: " + socket.id + " | Connected Total: " + io.engine.clientsCount)
+    //     setInterval(() => {
+    //         io.engine.emit("hi")
+    //         io.emit("hi")
+    //         console.log("emit")
+    //     }, 3000)
+    //
+    // });
+    //
+    //
+    //
+    //
+    //
+    // nitroApp.router.use("/socket.io", defineEventHandler({
+    //     handler(event) {
+    //         engine.handleRequest(event.node.req, event.node.res);
+    //         event._handled = true;
+    //     },
+    //     websocket: {
+    //         open(peer) {
+    //             // @ts-expect-error private method and property
+    //             engine.prepare(peer._internal.nodeReq);
+    //             // @ts-expect-error private method and property
+    //             engine.onWebSocket(peer._internal.nodeReq, peer._internal.nodeReq.socket, peer.websocket);
+    //         }
+    //     }
+    // }));
+    //
+    // setIO(io)
 
-    io.bind(engine);
-
+    const app = express();
+    const httpServer = createServer(app);
+    const io = new Server(httpServer, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    });
     io.on("connection", (socket) => {
         console.log("Connected Socket: " + socket.id + " | Connected Total: " + io.engine.clientsCount)
     });
-
-
-
-    nitroApp.router.use("/socket.io", defineEventHandler({
-        handler(event) {
-            engine.handleRequest(event.node.req, event.node.res);
-            event._handled = true;
-        },
-        websocket: {
-            open(peer) {
-                // @ts-expect-error private method and property
-                engine.prepare(peer._internal.nodeReq);
-                // @ts-expect-error private method and property
-                engine.onWebSocket(peer._internal.nodeReq, peer._internal.nodeReq.socket, peer.websocket);
-            }
-        }
-    }));
-
+    httpServer.listen(3001);
     setIO(io)
 });
