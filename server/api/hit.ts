a4d958ec6d571f9d1eaf3777424cfa03286715b3
Donald Zou <donaldzou@live.hk>
2025-06-21 04:04:48 +0800

    Update hit.ts

--- server/api/hit.ts
+++ server/api/hit.ts
@@ -4,7 +4,7 @@ import { v4 } from "uuid";
 import {getIO} from "~/server/utils/socket";
 
 export default defineEventHandler(async (event) => {
-	console.log(event.node.req.headers)
+	console.log(`URL: ${event.node.req.url} | IP: ${getRequestIP(event)}`)
 
 	const query = getQuery(event)
 	let currentCount = 1;
